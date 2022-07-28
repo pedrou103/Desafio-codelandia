79a2c41f930abab6f415356374c898994f2daf7f
pedrou103 <pedroluca13.plm40@gmail.com>
2022-07-28 13:48:50 -0300

    Desafio 12

---
+++ Desafio 12/src/assets/components/App/App.jsx
@@ -0,0 +1,18 @@
+import MainContent from '../MainContent/MainContent'
+import './App.sass'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>La P<span>i</span><span>z</span><span>z</span>a</h1>
+      </header>
+      <MainContent/>
+      <footer>
+        <p>todos os direitos reservados</p>
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ Desafio 12/src/assets/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import './Button.sass'
+
+export function Button({texto}) {
+    return (
+        <button>{texto}</button>
+    )
+}
