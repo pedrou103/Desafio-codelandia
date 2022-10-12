87d8aba98ef18c392e73b0309e6841ddd9cac83c
pedrou103 <pedroluca13.plm40@gmail.com>
2022-10-12 16:08:19 -0300

    configurando arquivo de configuração do vite para rodar projeto no celular

--- Desafio 12/vite.config.js
+++ Desafio 12/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: '3000',
+    hot: true
+  },
+
+  "scripts": {
+    "dev": "vite",
+    "prod": "vite build"
+  },
+
   plugins: [react()]
 })
